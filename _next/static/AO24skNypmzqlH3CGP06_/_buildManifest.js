ecadbafb8f83ad7e5af985d475968c10b8dd3852
threehams <threehams@users.noreply.github.com>
2022-01-02 01:10:02 +0000

    deploy: 67cd0572b0f6dc5fe9e338014906f385dd4bf3d7

---
+++ _next/static/AO24skNypmzqlH3CGP06_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7a7c95a0-20feb1995b92e319.js","static/chunks/418-209af34755bb1037.js","static/chunks/pages/index-63956ce7d6295205.js"],"/_error":["static/chunks/pages/_error-2e96aae4796b3cc2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
