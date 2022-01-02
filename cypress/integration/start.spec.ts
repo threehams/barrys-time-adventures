f9a3d364ac83e5c348542de79c4918ea2b326f08
David Edmondson <threehams@gmail.com>
2022-01-01 16:01:08 -0800

    one passing test

--- cypress/integration/start.spec.ts
+++ cypress/integration/start.spec.ts
@@ -1,16 +1,8 @@
 export {};
 
 describe("new game", () => {
-  it("completes the first day", () => {
+  it("starts up without issues", () => {
     cy.visit("/");
-    cy.findByRole("button", { name: /Closet/i }).click();
-    cy.findByRole("button", { name: /Wear jeans/i }).click();
-    cy.findByRole("button", { name: /Wear t-shirt/i }).click();
-    cy.findByRole("button", { name: /Wear socks/i }).click();
-    cy.findByRole("button", { name: /Wear briefs/i }).click();
-    cy.findByText("Go to work").should("not.be.disabled").click();
-    cy.findByText("Play a game").click();
-    cy.findByText("Go to sleep").click();
-    cy.findByText("Go to work");
+    cy.findByText("You have 0 things.");
   });
 });
