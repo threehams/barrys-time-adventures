4ffbb97c2554ebbfed6be8809a9c2f9aa30605e5
David Edmondson <threehams@gmail.com>
2022-01-09 23:51:44 -0800

    Add ability to reset game on crash

--- components/GameProvider.tsx
+++ components/GameProvider.tsx
@@ -3,6 +3,8 @@ import { useWorker } from "@laundry/worker";
 import { DispatchProvider, StateProvider } from "./StateProvider";
 import { Game } from "./Game";
 import { useRouter } from "next/router";
+import { ErrorBoundary } from "react-error-boundary";
+import { Reset } from "./Reset";
 
 const children = <Game />;
 
@@ -20,11 +22,23 @@ export const GameProvider = () => {
 
   return (
     <StateProvider value={state}>
-      <DispatchProvider value={dispatch}>{children}</DispatchProvider>
+      <DispatchProvider value={dispatch}>
+        <ErrorBoundary FallbackComponent={ErrorReset}>{children}</ErrorBoundary>
+      </DispatchProvider>
       {!process.env.NEXT_PUBLIC_DISABLE_CHEATS &&
         router.query.debug !== undefined && (
           <pre>{JSON.stringify(state, null, 2)}</pre>
         )}
     </StateProvider>
+  );
+};
+
+const ErrorReset = ({ error }) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-1 text-center">
+      Game crashes, likely an out-of-date save game. Save game migration is not
+      yet in place. Reset your game for now, then reload the page.
+      <Reset />
+    </div>
   );
 };
